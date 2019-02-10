31f36fa860bf704de5e64ca68011e884491f7e8e
FUAD\fuady <fuadmyoussef@gmail.com>
2019-02-10 16:55:19 -0500

    Restructuring project

--- app/src/main/java/cs2340/garbagecollection/spacetrader/views/MainActivity.java
+++ app/src/main/java/cs2340/garbagecollection/spacetrader/views/MainActivity.java
@@ -1,7 +1,9 @@
-package cs2340.garbagecollection.spacetrader;
+package cs2340.garbagecollection.spacetrader.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import cs2340.garbagecollection.spacetrader.R;
 
 public class MainActivity extends AppCompatActivity {
 
