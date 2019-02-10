92aa168e37173e02da77399e8dfa29d31128630b
FUAD\fuady <fuadmyoussef@gmail.com>
2019-02-10 16:38:12 -0500

    initial commit from android studio

---
+++ app/src/main/java/cs2340/garbagecollection/spacetrader/MainActivity.java
@@ -0,0 +1,13 @@
+package cs2340.garbagecollection.spacetrader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
