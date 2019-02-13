4dd460d4de683d24abc7b765e69c307b3eae78ed
jlatour3 <jlatour3@gatech.edu>
2019-02-13 14:04:28 -0500

    Added Player class private variables

--- app/src/main/java/cs2340/garbagecollection/spacetrader/model/Player.java
+++ app/src/main/java/cs2340/garbagecollection/spacetrader/model/Player.java
@@ -1,4 +1,20 @@
 package cs2340.garbagecollection.spacetrader.model;
 
 public class Player {
+    /** player name **/
+    private String name;
+
+    /** points for pilot **/
+    private int pilotPoints;
+
+    /** points for fighter **/
+    private int fighterPoints;
+
+    /** points for trader **/
+    private int traderPoints;
+
+    /** points for engineer **/
+    private int engineerPoints;
+
+
 }
