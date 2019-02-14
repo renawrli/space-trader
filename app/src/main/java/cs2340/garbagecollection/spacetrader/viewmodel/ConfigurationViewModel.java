c5daf4c44f1a727bd4bf969ff4ac4596f9468fff
jlatour3 <jlatour3@gatech.edu>
2019-02-14 12:49:25 -0500

    ConfigurationViewModel updated

--- app/src/main/java/cs2340/garbagecollection/spacetrader/viewmodel/ConfigurationViewModel.java
+++ app/src/main/java/cs2340/garbagecollection/spacetrader/viewmodel/ConfigurationViewModel.java
@@ -1,4 +1,20 @@
 package cs2340.garbagecollection.spacetrader.viewmodel;
 
-public class ConfigurationViewModel {
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.support.annotation.NonNull;
+
+import cs2340.garbagecollection.spacetrader.model.Player;
+
+public class ConfigurationViewModel extends AndroidViewModel {
+    private Player currentPlayer;
+
+    public ConfigurationViewModel (@NonNull Application application) {
+        super(application);
+    }
+
+    public void setCurrentPlayer(Player player) {
+        currentPlayer = player;
+    }
 }
