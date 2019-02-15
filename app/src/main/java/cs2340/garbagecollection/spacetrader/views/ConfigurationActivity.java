f4aa3e2dc245eae69170e49e9df450f8698fdafd
renawrli <renawrli@gmail.com>
2019-02-15 08:53:19 -0500

    Finished all point increment/decrement functionality in ConfigActivity

--- app/src/main/java/cs2340/garbagecollection/spacetrader/views/ConfigurationActivity.java
+++ app/src/main/java/cs2340/garbagecollection/spacetrader/views/ConfigurationActivity.java
@@ -18,13 +18,22 @@ import cs2340.garbagecollection.spacetrader.R;
 
 public class ConfigurationActivity extends AppCompatActivity {
     private ConfigurationViewModel configurationViewModel;
-    private int remPoints = 16;
-    private int difficultyLevel = 0;
+    private static final int MAX_POINTS = 16;
+    private int remPoints = MAX_POINTS;
+    private String difficultyLevel;
+
     private String playerName;
-    private int pilotPoints = 0;
+    private int fighterPoints;
+    private int pilotPoints;
+    private int traderPoints;
+    private int engineerPoints;
+
     private Spinner difficultySpinner;
     private TextView pDisplay;
     private TextView remPointsDisplay;
+    private TextView fighterDisplay;
+    private TextView traderDisplay;
+    private TextView engineerDisplay;
 
     private enum difficulties {
         EASY("Easy"), INTERMEDIATE("Intermediate"), EXPERT("Expert");
@@ -42,14 +51,73 @@ public class ConfigurationActivity extends AppCompatActivity {
         ArrayAdapter<String> difficultyArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, diffs);
         difficultyArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         difficultySpinner.setAdapter(difficultyArrayAdapter);
+
         pDisplay = (TextView) findViewById(R.id.pilotPointsDisplay);
+        traderDisplay = findViewById(R.id.traderPointsDisplay);
+        engineerDisplay = findViewById(R.id.engineerPointsDisplay);
+        fighterDisplay = findViewById(R.id.fighterPointsDisplay);
         remPointsDisplay = (TextView) findViewById(R.id.remainingPointsDisplay);
-
-
     }
 
-    protected void onSpinnerSelect() {
+    public void createPlayer(View view) {
+//        difficultyLevel = difficultySpinner.getPopupContext();
+    }
 
+    private void updatePointDisplays() {
+        pDisplay.setText(pilotPoints+"");
+        engineerDisplay.setText(engineerPoints+"");
+        traderDisplay.setText(traderPoints+"");
+        fighterDisplay.setText(fighterPoints+"");
+
+        remPointsDisplay.setText(remPoints+"");
+    }
+
+    public void decFighter(View view) {
+        if (fighterPoints > 0) {
+            fighterPoints--;
+            remPoints++;
+        }
+        updatePointDisplays();
+    }
+
+    public void incFighter(View view) {
+        if (remPoints > 0) {
+            fighterPoints++;
+            remPoints--;
+        }
+        updatePointDisplays();
+    }
+
+    public void decTrader(View view) {
+        if (traderPoints > 0) {
+            traderPoints--;
+            remPoints++;
+        }
+        updatePointDisplays();
+    }
+
+    public void incTrader(View view) {
+        if (remPoints > 0) {
+            traderPoints++;
+            remPoints--;
+        }
+        updatePointDisplays();
+    }
+
+    public void decEngineer(View view) {
+        if (engineerPoints > 0) {
+            engineerPoints--;
+            remPoints++;
+        }
+        updatePointDisplays();
+    }
+
+    public void incEngineer(View view) {
+        if (remPoints > 0) {
+            engineerPoints++;
+            remPoints--;
+        }
+        updatePointDisplays();
     }
 
     public void decPilot(View view) {
@@ -57,9 +125,7 @@ public class ConfigurationActivity extends AppCompatActivity {
             pilotPoints--;
             remPoints++;
         }
-
-        pDisplay.setText(pilotPoints+"");
-        remPointsDisplay.setText(remPoints+"");
+        updatePointDisplays();
     }
 
     public void incPilot(View view) {
@@ -67,10 +133,7 @@ public class ConfigurationActivity extends AppCompatActivity {
             pilotPoints++;
             remPoints--;
         }
-
-        pDisplay.setText(pilotPoints+"");
-        remPointsDisplay.setText(remPoints+"");
-        //create update display method
+        updatePointDisplays();
     }
 
 }
