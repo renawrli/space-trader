4b5ef38f37e8fc8f0a09cfd38570c4a5369e16b0
renawrli <renawrli@gmail.com>
2019-02-16 17:58:59 -0500

    Filled out GovernmentType enum

--- app/src/main/java/cs2340/garbagecollection/spacetrader/model/GovernmentType.java
+++ app/src/main/java/cs2340/garbagecollection/spacetrader/model/GovernmentType.java
@@ -1,5 +1,26 @@
 package cs2340.garbagecollection.spacetrader.model;
 
 public enum GovernmentType {
+    ANARCHY("Anarchy"),
+    CAPITALIST("Capitalist"),
+    COMMUNIST("Communist"),
+    CONFEDERACY("Confederacy"),
+    CORPORATE("Corporate"),
+    CYBERNETIC("Cybernetic"),
+    DEMOCRACY("Democracy"),
+    DICTATORISHIP("Dictatorship"),
+    FASCIST("Fascist"),
+    FEUDAL("Feudal"),
+    MILITARY("Military"),
+    MONARCHY("Monarchy"),
+    PACIFIST("Pacifist"),
+    SOCIALIST("Socialist"),
+    STATE_OF_SATORI("State of Satori"),
+    TECHNOCRACY("Technocracy"),
+    THEOCRACY("Theocracy");
 
+    private String governmentType;
+    GovernmentType(String governmentType) {
+        this.governmentType = governmentType;
+    }
 }
