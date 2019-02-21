bed43fe936c4c1c1775bdbe3fb1998c6641e4634
jlatour3 <jlatour3@gatech.edu>
2019-02-21 14:16:14 -0500

    Game class updated to instantiate the universe with solar systems

--- app/src/main/java/cs2340/garbagecollection/spacetrader/model/Game.java
+++ app/src/main/java/cs2340/garbagecollection/spacetrader/model/Game.java
@@ -1,5 +1,8 @@
 package cs2340.garbagecollection.spacetrader.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
     private Difficulty difficulty;
     private Player player;
@@ -10,9 +13,31 @@ public class Game {
      * @param difficulty - difficulty of the game
      * @param player - Player that plays the game
      */
-    public Game(Difficulty difficulty, Player player, Universe universe) {
+    public Game(Difficulty difficulty, Player player) {
         this.difficulty = difficulty;
         this.player = player;
-        this.universe = universe;
+        SolarSystem africa = new SolarSystem();
+        SolarSystem caribbean = new SolarSystem();
+        SolarSystem centralAmerica = new SolarSystem();
+        SolarSystem westEurope = new SolarSystem();
+        SolarSystem eastEurope = new SolarSystem();
+        SolarSystem northAmerica = new SolarSystem();
+        SolarSystem southAmerica = new SolarSystem();
+        SolarSystem centralAsia = new SolarSystem();
+        SolarSystem southEastAsia = new SolarSystem();
+        SolarSystem middleEast = new SolarSystem();
+        List<SolarSystem> theUniverse = new ArrayList<>();
+        theUniverse.add(africa);
+        theUniverse.add(caribbean);
+        theUniverse.add(centralAmerica);
+        theUniverse.add(westEurope);
+        theUniverse.add(eastEurope);
+        theUniverse.add(northAmerica);
+        theUniverse.add(southAmerica);
+        theUniverse.add(centralAsia);
+        theUniverse.add(southEastAsia);
+        theUniverse.add(middleEast);
     }
+
+
 }
