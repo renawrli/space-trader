5d2ddfe4ba082d0280ae29d453d6dbd76681f58f
renawrli <renawrli@gmail.com>
2019-02-20 22:17:54 -0500

    Fixed constructor errors in SolarSystem and added some getters and setters

--- app/src/main/java/cs2340/garbagecollection/spacetrader/model/SolarSystem.java
+++ app/src/main/java/cs2340/garbagecollection/spacetrader/model/SolarSystem.java
@@ -1,13 +1,29 @@
 package cs2340.garbagecollection.spacetrader.model;
 
 import java.util.HashSet;
+import java.util.List;
 
 public class SolarSystem {
 
     /** solar system's planets */
-    private HashSet<Planet>[] planets;
+    private HashSet<Planet> planets;
 
     SolarSystem() {
-        planets = new HashSet[10];
+        planets = new HashSet<Planet>(10);
+    }
+
+    /** constructs solar system from List of Planet objects **/
+    SolarSystem(List<Planet> planetList) {
+        planets = new HashSet<Planet>(planetList);
+    }
+
+    /** adds singular planet to the solar system **/
+    public void addPlanet(Planet planet) {
+        planets.add(planet);
+    }
+
+    /** returns number of planets in this solar system **/
+    public int getNumberOfPlanets() {
+        return planets.size();
     }
 }
