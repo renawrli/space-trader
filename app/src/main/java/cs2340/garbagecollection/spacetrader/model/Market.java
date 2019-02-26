8e9a64088c4999da50fc8ed3886fac0602b144ed
jlatour3 <jlatour3@gatech.edu>
2019-02-26 07:13:22 -0500

    Market class created.

---
+++ app/src/main/java/cs2340/garbagecollection/spacetrader/model/Market.java
@@ -0,0 +1,10 @@
+package cs2340.garbagecollection.spacetrader.model;
+
+public class Market {
+    /** location of market, x coordinate */
+    private final int x;
+
+    /** location of market, y coordinate */
+    private final int y;
+
+}
