3a3d1102c5a6d849a408f2721d082d8457a8e69c
jlatour3 <jlatour3@gatech.edu>
2019-02-26 13:12:34 -0500

    Adds remove method

--- app/src/main/java/cs2340/garbagecollection/spacetrader/model/Ship.java
+++ app/src/main/java/cs2340/garbagecollection/spacetrader/model/Ship.java
@@ -7,12 +7,23 @@ class Ship {
 
     /** represents actual cargo **/
     Object[] cargoArr;
+    int size = 0;
 
     Ship(ShipType shipType) {
         this.shipType = shipType;
         cargoArr = new Object[shipType.getCapacity()];
     }
 
-
+    /**
+     * Removes object sold from ship
+     * @param index that object is located in
+     * @return Object sold on ship
+     */
+    public Object remove(int index) {
+        Object sold = cargoArr[index];
+        size--;
+        cargoArr[index] = null;
+        return sold;
+    }
 
 }
