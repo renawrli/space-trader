3ffda2e3d2e37c49c31c400507f695f053807d5e
jlatour3 <jlatour3@gatech.edu>
2019-02-26 12:50:40 -0500

    ShipType has a cargo capacity variable

--- app/src/main/java/cs2340/garbagecollection/spacetrader/model/Ship.java
+++ app/src/main/java/cs2340/garbagecollection/spacetrader/model/Ship.java
@@ -2,6 +2,7 @@ package cs2340.garbagecollection.spacetrader.model;
 
 /** Represents a Ship owned by the Player **/
 class Ship {
+
     private ShipType shipType;
 
     Ship(ShipType shipType) {
--- app/src/main/java/cs2340/garbagecollection/spacetrader/model/ShipType.java
+++ app/src/main/java/cs2340/garbagecollection/spacetrader/model/ShipType.java
@@ -4,14 +4,29 @@ package cs2340.garbagecollection.spacetrader.model;
  * enum for the different types of Ships
  */
 public enum ShipType {
-    FLEA,
-    GNAT,
-    FIREFLY,
-    MOSQUITO,
-    BUMBLEBEE,
-    BEETLE,
-    HORNET,
-    GRASSHOPPER,
-    TERMITE,
-    WASP;
+    FLEA(5),
+    GNAT(15),
+    FIREFLY(20),
+    MOSQUITO(15),
+    BUMBLEBEE(20),
+    BEETLE(50),
+    HORNET(20),
+    GRASSHOPPER(30),
+    TERMITE(60),
+    WASP(35);
+
+    /** the ship's cargo capacity */
+    private int capacity;
+
+    ShipType(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
 }
