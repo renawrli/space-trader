6227caa0aaa5018af80e4dff5afc900b7bf70bf6
jlatour3 <jlatour3@gatech.edu>
2019-02-26 07:46:53 -0500

    Price variable added.

--- app/src/main/java/cs2340/garbagecollection/spacetrader/model/Market.java
+++ app/src/main/java/cs2340/garbagecollection/spacetrader/model/Market.java
@@ -7,6 +7,9 @@ public class Market {
     /** location of market, y coordinate */
     private final int y;
 
+    /** price of goods */
+    private double price;
+
     public Market(int x, int y) {
         this.x = x;
         this.y = y;
