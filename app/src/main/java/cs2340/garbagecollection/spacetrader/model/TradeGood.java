5ec29635fdbb42c8fc4034643bc14b7e4c996f22
jlatour3 <jlatour3@gatech.edu>
2019-02-26 10:33:19 -0500

    TradeGood ER variable updated to Resources instead of String

--- app/src/main/java/cs2340/garbagecollection/spacetrader/model/TradeGood.java
+++ app/src/main/java/cs2340/garbagecollection/spacetrader/model/TradeGood.java
@@ -7,16 +7,16 @@ public enum TradeGood {
     /** Each resource holds its name, MTLP, MTLU, TTP, Base Price,
         IPL,Var, IE, CR, ER, MTL, MTH respectively */
 
-    WATER(0, 0, 2, 30, 3, 4, "DROUGHT", Resources.LOTSOFWATER, "DESERT", 30, 50),
-    FURS(0, 0, 0, 250, 10, 10, "COLD", Resources.RICHFAUNA, "LIFELESS",	230, 280),
-    FOOD(1,0, 1, 100, 5, 5, "CROPFAIL",	Resources.RICHSOIL,	"POORSOIL", 90, 16),
-    ORE(2, 2, 3, 350, 20, 10, "WAR", Resources.MINERALRICH, "MINERALPOOR",350, 420),
-    GAMES(3, 1, 6, 250, -10, 5, "BOREDOM", Resources.ARTISTIC, "NEVER",	160, 270),
-    FIREARMS(3, 1, 5, 125, -75, 100, "WAR", Resources.WARLIKE, "NEVER", 600, 1100),
-    MEDICINE(4, 1, 6, 650, -20, 10, "PLAGUE", Resources.LOTSOFHERBS, "NEVER", 400, 700),
-    MACHINES(4, 3, 5, 900, -30, 5, "LACKOFWORKERS",	null, "NEVER",600, 800),
-    NARCOTICS(5, 0, 5, 3500, -125, 150, "BOREDOM", Resources.WEIRDMUSHROOMS, "NEVER", 2000, 3000),
-    ROBOTS( 6, 4, 7, 5000, -150, 100, "LACKOFWORKERS", null, "NEVER", 3500, 5000);
+    WATER(0, 0, 2, 30, 3, 4, "DROUGHT", Resources.LOTSOFWATER, Resources.DESERT, 30, 50),
+    FURS(0, 0, 0, 250, 10, 10, "COLD", Resources.RICHFAUNA, Resources.LIFELESS,	230, 280),
+    FOOD(1,0, 1, 100, 5, 5, "CROPFAIL",	Resources.RICHSOIL,	Resources.POORSOIL, 90, 16),
+    ORE(2, 2, 3, 350, 20, 10, "WAR", Resources.MINERALRICH, Resources.MINERALPOOR,350, 420),
+    GAMES(3, 1, 6, 250, -10, 5, "BOREDOM", Resources.ARTISTIC, null, 160, 270),
+    FIREARMS(3, 1, 5, 125, -75, 100, "WAR", Resources.WARLIKE, null, 600, 1100),
+    MEDICINE(4, 1, 6, 650, -20, 10, "PLAGUE", Resources.LOTSOFHERBS, null, 400, 700),
+    MACHINES(4, 3, 5, 900, -30, 5, "LACKOFWORKERS",	null, null,600, 800),
+    NARCOTICS(5, 0, 5, 3500, -125, 150, "BOREDOM", Resources.WEIRDMUSHROOMS, null, 2000, 3000),
+    ROBOTS( 6, 4, 7, 5000, -150, 100, "LACKOFWORKERS", null, null, 3500, 5000);
 
 
     private int MTLP;
@@ -27,13 +27,13 @@ public enum TradeGood {
     private int var;
     private String IE;
     private Resources CR;
-    private String ER;
+    private Resources ER;
     private int MTL;
     private int MTH;
 
 
     TradeGood(int MTLP, int MTLU, int TTP, double basePrice, int IPL, int var,
-               String IE, Resources CR, String ER, int MTL, int MTH) {
+               String IE, Resources CR, Resources ER, int MTL, int MTH) {
         this.MTLP = MTLP;
         this.MTLU = MTLU;
         this.TTP = TTP;
