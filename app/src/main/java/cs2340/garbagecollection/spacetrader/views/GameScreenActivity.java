23abb32a109b3bbb475718d20a485aa01e113b18
Fuad Youssef <fuadmyoussef@gmail.com>
2019-03-04 17:39:15 -0500

    added a lot of new translations changed list item to be easier to click and more readable added a filler current planet to GameActivity to make the string not hard coded. This is probably how we will determine the string welcoming the player to the new planet changed the layout of the dialog box to buy and sell

--- app/src/main/java/cs2340/garbagecollection/spacetrader/views/GameScreenActivity.java
+++ app/src/main/java/cs2340/garbagecollection/spacetrader/views/GameScreenActivity.java
@@ -5,15 +5,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import cs2340.garbagecollection.spacetrader.R;
+import cs2340.garbagecollection.spacetrader.model.Game;
+import cs2340.garbagecollection.spacetrader.model.Planet;
 
 public class GameScreenActivity extends AppCompatActivity {
-
+    TextView welcomeText;
+    Planet currentPlanet = Game.getUniverse().getAllSolarSystems().get(0).getAllPlanets().get(0);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_screen);
+        welcomeText = (TextView)findViewById(R.id.universeCreationLabel);
+        welcomeText.setText(welcomeText.getText() + " " + currentPlanet.getName());
     }
 
     public void toMarket(View view) {
