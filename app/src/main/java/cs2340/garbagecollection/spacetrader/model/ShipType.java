317a6edf330ec4ef603f41f686c99a4163b3f17a
jlatour3 <jlatour3@gatech.edu>
2019-03-12 21:12:08 -0400

    updated Ship Type to have maximum fuel capacity

--- app/src/main/java/cs2340/garbagecollection/spacetrader/model/ShipType.java
+++ app/src/main/java/cs2340/garbagecollection/spacetrader/model/ShipType.java
@@ -4,25 +4,33 @@ package cs2340.garbagecollection.spacetrader.model;
  * enum for the different types of Ships
  */
 public enum ShipType {
-    FLEA(5),
-    GNAT(15),
-    FIREFLY(20),
-    MOSQUITO(15),
-    BUMBLEBEE(20),
-    BEETLE(50),
-    HORNET(20),
-    GRASSHOPPER(30),
-    TERMITE(60),
-    WASP(35);
+    FLEA(5, 20),
+    GNAT(15, 14),
+    FIREFLY(20, 17),
+    MOSQUITO(15, 13),
+    BUMBLEBEE(20, 15),
+    BEETLE(50, 14),
+    HORNET(20, 16),
+    GRASSHOPPER(30, 15),
+    TERMITE(60, 13),
+    WASP(35, 14);
 
     /** the ship's cargo capacity */
     private int capacity;
 
-    ShipType(int capacity) {
+    /** the ship's fuel capacity */
+    private int fuelCapacity;
+
+    ShipType(int capacity, int fuelCapacity) {
         this.capacity = capacity;
+        this.fuelCapacity = fuelCapacity;
     }
 
     public int getCapacity() {
         return capacity;
+    }
+
+    public int getFuelCapacity() {
+        return fuelCapacity;
     }
 }
