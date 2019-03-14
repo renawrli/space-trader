da960234d6a3df15c0321b72a64cc6d77b307d00
andrew <zhang_andrew@yahoo.com>
2019-03-14 19:49:21 -0400

    added TravelViewModel and planetsInRange() in it

---
+++ app/src/main/java/cs2340/garbagecollection/spacetrader/viewmodel/TravelViewModel.java
@@ -0,0 +1,46 @@
+package cs2340.garbagecollection.spacetrader.viewmodel;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.support.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import cs2340.garbagecollection.spacetrader.model.Planet;
+import cs2340.garbagecollection.spacetrader.model.Ship;
+
+public class TravelViewModel extends AndroidViewModel {
+
+    public TravelViewModel (@NonNull Application application) {
+        super(application);
+    }
+
+    /** how many units in the universe you travel per unit of fuel **/
+    private final int DIST_PER_FUEL = 8;
+
+    /**
+     * Returns a list of Planets that you have enough fuel to travel to
+     * @param currPlanet - current planet you are on
+     * @param list - list of all planets
+     * @return list of Planets that are within range
+     */
+    public List<Planet> planetsInRange(Planet currPlanet, List<Planet> list, Ship currShip) {
+        // list of distances to each planet
+        List<Integer> distances = new ArrayList<>();
+        List<Planet> validPlanets = new ArrayList<>();
+
+        // max distance the ship can travel on current fuel tank
+        int travelableDist = currShip.getFuel() * DIST_PER_FUEL;
+
+        for (Planet planet: list) {
+            distances.add(currPlanet.calcDistance(planet));
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            if (travelableDist >= distances.get(i)) {
+                validPlanets.add(list.get(i));
+            }
+        }
+        return validPlanets;
+    }
+}
