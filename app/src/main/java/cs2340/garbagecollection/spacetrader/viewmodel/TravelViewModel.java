f7743a5bfc0d5696c9949f03f526454b34e37756
andrew <zhang_andrew@yahoo.com>
2019-03-17 13:10:25 -0400

    updated TravelViewModel w/ methods to return Lists for TravelActivity

--- app/src/main/java/cs2340/garbagecollection/spacetrader/viewmodel/TravelViewModel.java
+++ app/src/main/java/cs2340/garbagecollection/spacetrader/viewmodel/TravelViewModel.java
@@ -24,25 +24,55 @@ public class TravelViewModel extends AndroidViewModel {
     /**
      * Returns a list of Planets that you have enough fuel to travel to
      * @param currPlanet - current planet you are on
-     * @param list - list of all planets
+     * @param planetList - list of all planets
      * @return list of Planets that are within range
      */
-    public List<Planet> planetsInRange(Planet currPlanet, List<Planet> list, Ship currShip) {
-        // list of distances to each planet
-        List<Integer> distances = new ArrayList<>();
-        List<Planet> validPlanets = new ArrayList<>();
+    public List<Planet> planetsInRange(Planet currPlanet, List<Planet> planetList, Ship currShip) {
         // max distance the ship can travel on current fuel tank
         int travelableDist = currShip.getFuel() * DIST_PER_FUEL;
+        // list of distances to each planet
+        List<Integer> distances;
+        List<Planet> validPlanets = new ArrayList<>();
 
-        for (Planet planet: list) {
-            distances.add(currPlanet.calcDistance(planet));
-        }
-        for (int i = 0; i < list.size(); i++) {
+        distances = listDistances(currPlanet, planetList);
+
+        for (int i = 0; i < distances.size(); i++) {
             if (travelableDist >= distances.get(i)) {
-                validPlanets.add(list.get(i));
+                validPlanets.add(planetList.get(i));
             }
         }
         return validPlanets;
+    }
+
+    /** Returns a list of distances from currPlanet to each planet in planetList **/
+    public List<Integer> listDistances(Planet currPlanet, List<Planet> planetList) {
+        List<Integer> distances = new ArrayList<>();
+        for (Planet planet: planetList) {
+            distances.add(currPlanet.calcDistance(planet));
+        }
+        return distances;
+    }
+
+    /**
+     * Returns a list of how much fuel it takes to travel to each planet
+     * @param distances - list of distances to each planet
+     * @return a list of fuel units it will cost to fly to each destination
+     */
+    public List<Integer> listFuel(List<Integer> distances) {
+        List<Integer> fuelList = new ArrayList<>();
+        for(Integer i: distances) {
+            fuelList.add(i / DIST_PER_FUEL);
+        }
+        return fuelList;
+    }
+
+    /** takes in a list of valid planets and returns them in string format **/
+    public List<String> validPlanetsString(List<Planet> validPlanets) {
+        ArrayList<String> planetNames = new ArrayList<>();
+        for (int i = 0; i < validPlanets.size(); i++) {
+            planetNames.add(validPlanets.get(i).getName());
+        }
+        return planetNames;
     }
 
     /** travels to a Planet. Assumes it's within range **/
