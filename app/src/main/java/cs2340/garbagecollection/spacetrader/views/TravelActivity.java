e2d19acf3251b0e0fb66f8023ab2fc68fe7082f9
Fuad Youssef <fuadmyoussef@gmail.com>
2019-03-17 19:30:43 -0400

    Actually added TravelActivity

---
+++ app/src/main/java/cs2340/garbagecollection/spacetrader/views/TravelActivity.java
@@ -0,0 +1,22 @@
+package cs2340.garbagecollection.spacetrader.views;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import cs2340.garbagecollection.spacetrader.R;
+
+
+public class TravelActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.travel_screen);
+
+
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}
