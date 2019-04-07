3176c0ddbc00b8956f8e440807cb9353b5a2281f
Philena Yang <philenayang@lawn-128-61-25-245.lawn.gatech.edu>
2019-04-07 14:40:46 -0400

    finished Philena JUnits

--- app/src/test/java/cs2340/garbagecollection/spacetrader/MarketSellableGoodsTest.java
+++ app/src/test/java/cs2340/garbagecollection/spacetrader/MarketSellableGoodsTest.java
@@ -14,94 +14,89 @@ import static org.junit.Assert.*;
 public class MarketSellableGoodsTest {
     @Test
     public void getAllSellableGoods() {
-        //the planets made to test
-        Planet testPlanet1 = new Planet("Test", 1, 1, TechLevel.HiTech, GovernmentType.ANARCHY, Resources.NOSPECIALRESOURCES, 1,1);
-        Planet testPlanet2 = new Planet("Test", 1, 1, TechLevel.PreAgriculture, GovernmentType.ANARCHY, Resources.NOSPECIALRESOURCES, 1,1);
-        Planet testPlanet3 = new Planet("Test", 1, 1, TechLevel.Renaissance, GovernmentType.ANARCHY, Resources.NOSPECIALRESOURCES, 1,1);
-        Planet testPlanet4 = new Planet("Test", 1, 1, TechLevel.Agriculture, GovernmentType.ANARCHY, Resources.NOSPECIALRESOURCES, 1,1);
-        Planet testPlanet5 = new Planet("Test", 1, 1, TechLevel.Medieval, GovernmentType.ANARCHY, Resources.NOSPECIALRESOURCES, 1,1);
-        Planet testPlanet6 = new Planet("Test", 1, 1, TechLevel.EarlyIndustrial, GovernmentType.ANARCHY, Resources.NOSPECIALRESOURCES, 1,1);
-        Planet testPlanet7 = new Planet("Test", 1, 1, TechLevel.Industrial, GovernmentType.ANARCHY, Resources.NOSPECIALRESOURCES, 1,1);
-        Planet testPlanet8 = new Planet("Test", 1, 1, TechLevel.PostIndustrial, GovernmentType.ANARCHY, Resources.NOSPECIALRESOURCES, 1,1);
-        List<TradeGood> expectPlanet1 = new ArrayList<>();
-        //adds all trade goods to expected planet 1 because all goods are buyable at planet 1
-        //also the expected list for post Industrial planets so same for planet 8
+
+        Planet planetA = new Planet("Test", 1, 1, TechLevel.HiTech, GovernmentType.ANARCHY, Resources.NOSPECIALRESOURCES, 1,1);
+        Planet planetB = new Planet("Test", 1, 1, TechLevel.PreAgriculture, GovernmentType.ANARCHY, Resources.NOSPECIALRESOURCES, 1,1);
+        Planet planetC = new Planet("Test", 1, 1, TechLevel.Renaissance, GovernmentType.ANARCHY, Resources.NOSPECIALRESOURCES, 1,1);
+        Planet planetD = new Planet("Test", 1, 1, TechLevel.Agriculture, GovernmentType.ANARCHY, Resources.NOSPECIALRESOURCES, 1,1);
+        Planet planetE = new Planet("Test", 1, 1, TechLevel.Medieval, GovernmentType.ANARCHY, Resources.NOSPECIALRESOURCES, 1,1);
+        Planet planetF = new Planet("Test", 1, 1, TechLevel.EarlyIndustrial, GovernmentType.ANARCHY, Resources.NOSPECIALRESOURCES, 1,1);
+        Planet planetG = new Planet("Test", 1, 1, TechLevel.Industrial, GovernmentType.ANARCHY, Resources.NOSPECIALRESOURCES, 1,1);
+        Planet planetH = new Planet("Test", 1, 1, TechLevel.PostIndustrial, GovernmentType.ANARCHY, Resources.NOSPECIALRESOURCES, 1,1);
+        List<TradeGood> ePlanet1 = new ArrayList<>();
+
+        //adds trade goods to expected planet 1
         for (TradeGood good : TradeGood.values()) {
-            expectPlanet1.add(good);
+            ePlanet1.add(good);
         }
-        List<TradeGood> expectPlanet2 = new ArrayList<>();
-        //planet 2 should only be able to sell water and furs
-        expectPlanet2.add(TradeGood.WATER);
-        expectPlanet2.add(TradeGood.FURS);
 
-        List<TradeGood> expectPlanet3 = new ArrayList<>();
-        //planet 3 should be able to sell water, furs, ore, games, and firearms
-        expectPlanet3.add(TradeGood.WATER);
-        expectPlanet3.add(TradeGood.FURS);
-        expectPlanet3.add(TradeGood.FOOD);
-        expectPlanet3.add(TradeGood.ORE);
-        expectPlanet3.add(TradeGood.GAMES);
-        expectPlanet3.add(TradeGood.FIREARMS);
+        List<TradeGood> ePlanet2 = new ArrayList<>();
+        ePlanet2.add(TradeGood.WATER);
+        ePlanet2.add(TradeGood.FURS);
 
-        List<TradeGood> expectPlanet4 = new ArrayList<>();
-        //planet 4 should be able to sell water, furs, and food
-        expectPlanet4.add(TradeGood.WATER);
-        expectPlanet4.add(TradeGood.FURS);
-        expectPlanet4.add(TradeGood.FOOD);
+        List<TradeGood> ePlanet3 = new ArrayList<>();
+        ePlanet3.add(TradeGood.WATER);
+        ePlanet3.add(TradeGood.FURS);
+        ePlanet3.add(TradeGood.FOOD);
+        ePlanet3.add(TradeGood.ORE);
+        ePlanet3.add(TradeGood.GAMES);
+        ePlanet3.add(TradeGood.FIREARMS);
 
-        List<TradeGood> expectPlanet5 = new ArrayList<>();
-        //planet 5 should be able to sell water, furs, food, and ore
-        expectPlanet5.add(TradeGood.WATER);
-        expectPlanet5.add(TradeGood.FURS);
-        expectPlanet5.add(TradeGood.FOOD);
-        expectPlanet5.add(TradeGood.ORE);
+        List<TradeGood> ePlanet4 = new ArrayList<>();
+        ePlanet4.add(TradeGood.WATER);
+        ePlanet4.add(TradeGood.FURS);
+        ePlanet4.add(TradeGood.FOOD);
 
-        List<TradeGood> expectPlanet6 = new ArrayList<>();
-        //planet 6 should be able to sell water, furs, food, ore, games, firearms, medicine, and machines
-        expectPlanet6.add(TradeGood.WATER);
-        expectPlanet6.add(TradeGood.FURS);
-        expectPlanet6.add(TradeGood.FOOD);
-        expectPlanet6.add(TradeGood.ORE);
-        expectPlanet6.add(TradeGood.GAMES);
-        expectPlanet6.add(TradeGood.FIREARMS);
-        expectPlanet6.add(TradeGood.MEDICINE);
-        expectPlanet6.add(TradeGood.MACHINES);
+        List<TradeGood> ePlanet5 = new ArrayList<>();
+        ePlanet5.add(TradeGood.WATER);
+        ePlanet5.add(TradeGood.FURS);
+        ePlanet5.add(TradeGood.FOOD);
+        ePlanet5.add(TradeGood.ORE);
 
-        List<TradeGood> expectPlanet7 = new ArrayList<>();
-        //planet 7 should be able to sell water, furs, food, ore, games, firearms, medicine, machines, and narcotics
-        expectPlanet7.add(TradeGood.WATER);
-        expectPlanet7.add(TradeGood.FURS);
-        expectPlanet7.add(TradeGood.FOOD);
-        expectPlanet7.add(TradeGood.ORE);
-        expectPlanet7.add(TradeGood.GAMES);
-        expectPlanet7.add(TradeGood.FIREARMS);
-        expectPlanet7.add(TradeGood.MEDICINE);
-        expectPlanet7.add(TradeGood.MACHINES);
-        expectPlanet7.add(TradeGood.NARCOTICS);
+        List<TradeGood> ePlanet6 = new ArrayList<>();
+        ePlanet6.add(TradeGood.WATER);
+        ePlanet6.add(TradeGood.FURS);
+        ePlanet6.add(TradeGood.FOOD);
+        ePlanet6.add(TradeGood.ORE);
+        ePlanet6.add(TradeGood.GAMES);
+        ePlanet6.add(TradeGood.FIREARMS);
+        ePlanet6.add(TradeGood.MEDICINE);
+        ePlanet6.add(TradeGood.MACHINES);
 
-        List<TradeGood> actualPlanet1 = Market.getAllSellableGoods(testPlanet1.getTechnology());
-        assertEquals(expectPlanet1, actualPlanet1);
+        List<TradeGood> ePlanet7 = new ArrayList<>();
+        ePlanet7.add(TradeGood.WATER);
+        ePlanet7.add(TradeGood.FURS);
+        ePlanet7.add(TradeGood.FOOD);
+        ePlanet7.add(TradeGood.ORE);
+        ePlanet7.add(TradeGood.GAMES);
+        ePlanet7.add(TradeGood.FIREARMS);
+        ePlanet7.add(TradeGood.MEDICINE);
+        ePlanet7.add(TradeGood.MACHINES);
+        ePlanet7.add(TradeGood.NARCOTICS);
 
-        List<TradeGood> actualPlanet2 = Market.getAllSellableGoods(testPlanet2.getTechnology());
-        assertEquals(expectPlanet2, actualPlanet2);
+        List<TradeGood> actualPlanet1 = Market.getAllSellableGoods(planetA.getTechnology());
+        assertEquals(ePlanet1, actualPlanet1);
 
-        List<TradeGood> actualPlanet3 = Market.getAllSellableGoods(testPlanet3.getTechnology());
-        assertEquals(expectPlanet3, actualPlanet3);
+        List<TradeGood> actualPlanet2 = Market.getAllSellableGoods(planetB.getTechnology());
+        assertEquals(ePlanet2, actualPlanet2);
 
-        List<TradeGood> actualPlanet4 = Market.getAllSellableGoods(testPlanet4.getTechnology());
-        assertEquals(expectPlanet4, actualPlanet4);
+        List<TradeGood> actualPlanet3 = Market.getAllSellableGoods(planetC.getTechnology());
+        assertEquals(ePlanet3, actualPlanet3);
 
-        List<TradeGood> actualPlanet5 = Market.getAllSellableGoods(testPlanet5.getTechnology());
-        assertEquals(expectPlanet5, actualPlanet5);
+        List<TradeGood> actualPlanet4 = Market.getAllSellableGoods(planetD.getTechnology());
+        assertEquals(ePlanet4, actualPlanet4);
 
-        List<TradeGood> actualPlanet6 = Market.getAllSellableGoods(testPlanet6.getTechnology());
-        assertEquals(expectPlanet6, actualPlanet6);
+        List<TradeGood> actualPlanet5 = Market.getAllSellableGoods(planetE.getTechnology());
+        assertEquals(ePlanet5, actualPlanet5);
 
-        List<TradeGood> actualPlanet7 = Market.getAllSellableGoods(testPlanet7.getTechnology());
-        assertEquals(expectPlanet7, actualPlanet7);
+        List<TradeGood> actualPlanet6 = Market.getAllSellableGoods(planetF.getTechnology());
+        assertEquals(ePlanet6, actualPlanet6);
 
-        List<TradeGood> actualPlanet8 = Market.getAllSellableGoods(testPlanet8.getTechnology());
-        assertEquals(expectPlanet1, actualPlanet8);
+        List<TradeGood> actualPlanet7 = Market.getAllSellableGoods(planetG.getTechnology());
+        assertEquals(ePlanet7, actualPlanet7);
+
+        List<TradeGood> actualPlanet8 = Market.getAllSellableGoods(planetH.getTechnology());
+        assertEquals(ePlanet1, actualPlanet8);
 
     }
 }
