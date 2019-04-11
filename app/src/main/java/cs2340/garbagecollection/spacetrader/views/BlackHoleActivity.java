56c640b7f182d6a9bac3450cf1fff125fd0d294c
Philena Yang <philenayang@lawn-128-61-25-245.lawn.gatech.edu>
2019-04-11 13:13:20 -0400

    Added BlackHole and Drought ViewModel, Activity, and XML

---
+++ app/src/main/java/cs2340/garbagecollection/spacetrader/views/BlackHoleActivity.java
@@ -0,0 +1,42 @@
+package cs2340.garbagecollection.spacetrader.views;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import cs2340.garbagecollection.spacetrader.R;
+import cs2340.garbagecollection.spacetrader.viewmodel.BlackHoleViewModel;
+
+import static cs2340.garbagecollection.spacetrader.views.ConfigurationActivity.game;
+
+public class BlackHoleActivity extends AppCompatActivity {
+    BlackHoleViewModel blackHoleVM;
+    TextView banner;
+    TextView encounterType;
+    TextView encounterStatement;
+    Button exitButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.blackhole_encounter);
+        Log.d("onCreate", "onCreate: drought activity launched");
+
+        blackHoleVM = new BlackHoleViewModel(getApplication());
+
+        banner = findViewById(R.id.blackhole_encounter_banner_text);
+        encounterType = findViewById(R.id.blackhole_label);
+        encounterStatement = findViewById(R.id.blackhole_encounter_statement);
+        exitButton = findViewById(R.id.leaveBlackHoleButton);
+
+    }
+
+
+    public void exitBlackHole(View view) {
+        blackHoleVM.fuelDecrease(game.getPlayer().getShip());
+        finish();
+    }
+}
---
+++ app/src/main/java/cs2340/garbagecollection/spacetrader/views/DroughtActivity.java
@@ -0,0 +1,41 @@
+package cs2340.garbagecollection.spacetrader.views;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import cs2340.garbagecollection.spacetrader.R;
+import cs2340.garbagecollection.spacetrader.viewmodel.DroughtViewModel;
+
+public class DroughtActivity extends AppCompatActivity {
+    DroughtViewModel droughtVM;
+    TextView banner;
+    TextView encounterType;
+    TextView encounterStatement;
+    Button exitButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.drought_encounter);
+        Log.d("onCreate", "onCreate: drought activity launched");
+
+        droughtVM = new DroughtViewModel(getApplication());
+
+        banner = findViewById(R.id.drought_encounter_banner_text);
+        encounterType = findViewById(R.id.drought_label);
+        encounterStatement = findViewById(R.id.drought_encounter_statement);
+        exitButton = findViewById(R.id.leaveDroughtButton);
+
+    }
+
+
+    public void exitDrought(View view) {
+        //insert method to increase water price
+
+        finish();
+    }
+}
